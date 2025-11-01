ef52b58dde3ad44558433cb763603a05c0163921
AbeTakaki <abetakaki0101@gmail.com>
2025-11-02 08:55:59 +0900

    component化①

---
+++ my-react-app/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import Navgation from "./Navgation"
+
+const Footer = () => {
+  return (
+    <footer>
+      <Navgation />
+      <small>&copy; 2025</small>
+    </footer>
+  )
+}
+
+export default Footer
